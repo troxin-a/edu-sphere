ccbed37e388383c229204874584a8269927ff04d
Anton Pashinov <troxin1988@gmail.com>
2024-09-11 16:17:20 +0700

    Custom serializers

--- edu/serializers.py
+++ edu/serializers.py
@@ -3,13 +3,19 @@ from rest_framework import serializers
 from edu.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ("name", "description", "preview", "count_lessons", "lessons")
+
+    def get_count_lessons(self, course):
+        return Lesson.objects.filter(course__id=course.id).count()
